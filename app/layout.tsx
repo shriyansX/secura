494411bd88d652c87fe3540f499e69a431c846ed
Shriyans <shriyans696@gmail.com>
2025-08-02 11:21:40 +0530

    21 mins done

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -34,8 +35,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Navbar />
           <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
+            {/* <SignedOut>
               <SignInButton />
               <SignUpButton>
                 <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
@@ -45,7 +47,7 @@ export default function RootLayout({
             </SignedOut>
             <SignedIn>
               <UserButton />
-            </SignedIn>
+            </SignedIn> */}
           </header>
           {children}
         </body>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <nav className='flex justify-between items-center px-4 h-16 bg-border text-background'>
+            <span>Secura</span>
+            <ul className='flex gap-5 justify-start items-center'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
